d48075164a077fced3f34549c0de76384d9aa616
Yash Bajaj <bajajy82@gmail.com>
2023-09-08 17:21:17 +0530

    updated acao

--- src/index.ts
+++ src/index.ts
@@ -34,7 +34,7 @@ app.use(
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Origin", req.headers.origin);
+  res.header("Access-Control-Allow-Origin", "http://localhost:1420");
   res.header(
     "Access-Control-Allow-Methods",
     "GET,PUT,POST,DELETE,UPDATE,OPTIONS"
